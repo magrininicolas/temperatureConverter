fd9c6658afb41a6c4814a21a41136397df5f3cc4
Nicolas <magrininicolas@outlook.com>
2023-01-03 10:32:43 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+function converteTemperatura(){
+    const resposta = prompt("Deseja converter de qual medida? (C/F)");
+    if (resposta === '') return alert('Digite alguma medida.');
+    const temperatura = Number(prompt("Qual o valor da temperatura que deseja converter?"));
+    const resultado = document.querySelector('.resultado');
+    resultado.innerText = '';
+
+    if (resposta === 'C'){
+        function celsius2Fahrenheit(){
+            const fahrenheit = temperatura * 1.8 + 32;
+            resultado.innerText += `Sua temperatura de Celsius para Fahrenheit é igual a ${fahrenheit}°F.`;
+        }celsius2Fahrenheit();
+    }
+    if(resposta === 'F'){
+        function fahrenheit2Celsius(){
+            const celsius = (temperatura - 32) * 5/9;  
+            resultado.innerText += `Sua temperatura de Fahrenheit para Celsius é igual a ${celsius}°C.`;
+        }fahrenheit2Celsius();
+    }
+}
+converteTemperatura();
+
+
+
